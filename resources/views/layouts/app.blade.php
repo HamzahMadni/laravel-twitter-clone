d4f57bd4340bf71daf9230b591592fa39aaf715a
hamzah <hamzah.madni01@gmail.com>
2023-01-12 21:06:06 +0000

    setup base template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Posty</title>
-</head>
-<body>
-    @yield('content')
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite('resources/css/app.css')
+        <title>Posty</title>
+    </head>
+    <body class="bg-gray-200">
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Post</a>
+                </li>
+            </ul>
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3">John Doe</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Register</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            </ul>
+
+        </nav>
+        @yield('content')
+    </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    Index
+    <div class="class flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            Posts
+        </div>
+    </div>
 @endsection
