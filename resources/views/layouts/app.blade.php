1ec40756cc76d35da12c514b0ffd856940130b76
hamzah <hamzah.madni01@gmail.com>
2023-01-13 13:31:39 +0000

    apply auth and guest middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
             <ul class="flex items-center">
                 @auth
                     <li>
-                        <a href="" class="p-3">John Doe</a>
+                        <a href="" class="p-3">{{ auth()->user()->name }}</a>
                     </li>
                     <li>
                         <form action="{{ route('logout') }}" method="POST" class="p-3 inline">
