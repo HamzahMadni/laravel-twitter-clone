4350017460920990bfb88a895f51066cfa6a1e5d
hamzah <hamzah.madni01@gmail.com>
2023-01-13 09:33:21 +0000

    add login functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
                 
                 @guest
                     <li>
-                        <a href="" class="p-3">Login</a>
+                        <a href="{{ route('login')}}" class="p-3">Login</a>
                     </li>
                     <li>
                         <a href="{{ route('register')}}" class="p-3">Register</a>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])-> name('dashboard');
+
+Route::get('/login', [LoginController::class, 'index'])-> name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/register', [RegisterController::class, 'index'])-> name('register');
 Route::post('/register', [RegisterController::class, 'store']);
