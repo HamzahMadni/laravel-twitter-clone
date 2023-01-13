4d1c6f10a7e04e9626ec5e4fa323e243c0498de3
hamzah <hamzah.madni01@gmail.com>
2023-01-13 09:48:05 +0000

    add logout functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
         <nav class="p-6 bg-white flex justify-between mb-6">
             <ul class="flex items-center">
                 <li>
-                    <a href="" class="p-3">Home</a>
+                    <a href="/" class="p-3">Home</a>
                 </li>
                 <li>
-                    <a href="" class="p-3">Dashboard</a>
+                    <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
                 </li>
                 <li>
                     <a href="" class="p-3">Post</a>
@@ -26,7 +26,10 @@
                         <a href="" class="p-3">John Doe</a>
                     </li>
                     <li>
-                        <a href="" class="p-3">Logout</a>
+                        <form action="{{ route('logout') }}" method="POST" class="p-3 inline">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
                     </li>
                 @endauth
                 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('home');
+})-> name('home');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])-> name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])-> name('logout');
 
 Route::get('/login', [LoginController::class, 'index'])-> name('login');
 Route::post('/login', [LoginController::class, 'store']);
