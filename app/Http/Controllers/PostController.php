d4d1e49e827378c09796d4b4bd51180cd129a86c
hamzah <hamzah.madni01@gmail.com>
2023-01-13 15:00:28 +0000

    list posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::get();
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
     public function store(Request $request)
     {
-        dd('ok');
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="class flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post">
+            <form action="{{ route('posts') }}" method="post" class="mb-4">
                 @csrf
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
@@ -19,8 +19,19 @@
                 <div>
                     <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
                 </div>
-
             </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name}}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans()}}</span>    
+                    
+                        <p class="mb-2">{{ $post->body }}</p>
+                    </div>    
+                @endforeach
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
