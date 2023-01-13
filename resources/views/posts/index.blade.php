e7bbd68a65d7273f9309dedd8f10741b527a3dd3
hamzah <hamzah.madni01@gmail.com>
2023-01-13 17:23:33 +0000

    add eager loading

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,21 +29,21 @@
                         <p class="mb-2">{{ $post->body }}</p>
 
                         <div class="flex items-center">
-                            @if (!$post->likedBy(auth()->user()))
-                                <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Like</button>
-                                </form>
-                            @else
-                                <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-blue-500">Unlike</button>
-                                </form>
-                            @endif
+                            @auth
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth                         
                             
-                            
-
                             <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                         </div>
                     </div>    
