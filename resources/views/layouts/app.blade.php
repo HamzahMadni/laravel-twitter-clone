46fc2195b70e510f3ee668084f6e3c5f3473890e
hamzah <hamzah.madni01@gmail.com>
2023-01-15 20:02:47 +0000

    mobile menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,26 +8,26 @@
         <title>Twitter Clone</title>
     </head>
     <body class="bg-teal-700">
-        <nav class="py-2 px-6 bg-teal-900 flex justify-between mb-6 text-white">
+        <nav class="py-4 px-6 bg-teal-900 flex justify-between mb-6 text-white">
             <ul class="flex items-center space-x-2">
                 <li>
                     <a
                         href="{{ route('home') }}"
-                        class="p-3 rounded hover:bg-white/20 @if(request()->routeIs('home')) {{'font-bold bg-white/10'}} @endif"
+                        class="text-sm sm:text-base px-2 py-1 sm:p-3 rounded hover:bg-white/20 @if(request()->routeIs('home')) {{'font-bold bg-white/10'}} @endif"
                     >Home</a>
                 </li>
                 <li>
-                    <a href="{{ route('explore.index') }}" class="p-3 rounded hover:bg-white/20 @if(request()->routeIs('explore.index')) {{'font-bold bg-white/10'}} @endif">Explore</a>
+                    <a href="{{ route('explore.index') }}" class="text-sm sm:text-base px-2 py-1 sm:p-3 rounded hover:bg-white/20 @if(request()->routeIs('explore.index')) {{'font-bold bg-white/10'}} @endif">Explore</a>
                 </li>
             </ul>
-            <div class="p-2 font-bold text-3xl">Twitter Clone</div>
+            <div class="hidden sm:block font-bold text-3xl">Twitter Clone</div>
             <ul class="flex items-center space-x-2">
                 @auth
                     <li>
-                        <a href="{{ route('users.profile', auth()->user())}}" class="p-3 rounded hover:bg-white/20 @if(request()->routeIs('users.profile')) {{'font-bold bg-white/10'}} @endif">{{ auth()->user()->name }}</a>
+                        <a href="{{ route('users.profile', auth()->user())}}" class="text-sm sm:text-base px-2 py-1 sm:p-3 rounded hover:bg-white/20 @if(request()->routeIs('users.profile')) {{'font-bold bg-white/10'}} @endif">{{ auth()->user()->name }}</a>
                     </li>
                     <li>
-                        <form action="{{ route('logout') }}" method="POST" class="p-3 rounded hover:bg-white/20">
+                        <form action="{{ route('logout') }}" method="POST" class="text-sm sm:text-base px-2 py-1 sm:p-3 rounded hover:bg-white/20">
                             @csrf
                             <button type="submit">Logout</button>
                         </form>
@@ -36,10 +36,10 @@
                 
                 @guest
                     <li>
-                        <a href="{{ route('login')}}" class="p-3 rounded hover:bg-white/20 @if(request()->routeIs('login')) {{'font-bold bg-white/10'}} @endif">Login</a>
+                        <a href="{{ route('login')}}" class="text-sm sm:text-base px-2 py-1 sm:p-3 rounded hover:bg-white/20 @if(request()->routeIs('login')) {{'font-bold bg-white/10'}} @endif">Login</a>
                     </li>
                     <li>
-                        <a href="{{ route('register')}}" class="p-3 rounded hover:bg-white/20 @if(request()->routeIs('register')) {{'font-bold bg-white/10'}} @endif">Register</a>
+                        <a href="{{ route('register')}}" class="text-sm sm:text-base px-2 py-1 sm:p-3 rounded hover:bg-white/20 @if(request()->routeIs('register')) {{'font-bold bg-white/10'}} @endif">Register</a>
                     </li>
                 @endguest
 
