0bce5a4ab1d8d6201020a10e665087364d131a00
hamzah <hamzah.madni01@gmail.com>
2023-01-15 17:00:31 +0000

    use ajax to implement live search

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,21 @@
 import './bootstrap';
+import { debounce } from 'lodash';
+
+// Explore Search
+const searchInput = document.getElementById('username');
+
+if (searchInput) {
+    const url = new URL(location);
+    searchInput.value = url.searchParams.get('username')
+    
+    const search = () => {
+        const url = new URL(location);
+        url.searchParams.set("username", searchInput.value);
+        location = url.href;
+    };
+    
+    searchInput?.addEventListener('input', debounce(search, 300))
+}
 
 // Profile Tabs
 const tabContainer = document.getElementById('tabContainer');
--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -10,12 +10,12 @@
                         name="username"
                         value="{{ $username }}"
                         id="username"
-                        class="bg-gray-100 border-2 focus-visible:outline-teal-500 w-full px-4 py-2 rounded mr-4 @error('username') border-red-500 @enderror"
+                        class="bg-gray-100 border-2 focus-visible:outline-teal-500 w-full px-4 py-2 rounded @error('username') border-red-500 @enderror"
                         placeholder="Find a friend..."
                         onfocus="this.setSelectionRange(this.value.length,this.value.length)"
                         autofocus
                     />
-                    <button type="submit" class="inline bg-teal-900 hover:bg-teal-800 text-white px-4 py-2 rounded font-medium sm:w-fit">Search</button>
+                    {{-- <button type="submit" class="inline bg-teal-900 hover:bg-teal-800 text-white px-4 py-2 rounded font-medium sm:w-fit">Search</button> --}}
                 </form>
                 @error('username')
                     <div class="text-red-500 mt-2 text-sm">
@@ -23,15 +23,15 @@
                     </div>
                 @enderror
             </div>
-            <div class="flex flex-col justify-between items-center space-y-2 mt-4">
-                @if ($users->count())                        
+            @if ($users->count())                        
+                <div class="flex flex-col justify-between items-center space-y-2 mt-4">
                     @foreach ($users as $user)
                         <x-user :user="$user" />
                     @endforeach
-                @elseif (request()->query('username'))
-                    <p class="text-red-500 mt-4">No users with that name.</p>
-                @endif
-            </div>
+                </div>
+            @elseif (request()->query('username'))
+                <p class="text-red-500 mt-4">No users with that name.</p>
+            @endif
         </div>
         <div class="w-full sm:w-8/12 bg-white p-6 rounded-lg space-y-4">
             @if ($posts->count())
